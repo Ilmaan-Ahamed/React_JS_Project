a04ca1fa38c6039249a9a8013c695d1d6d918f70
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-05-20 00:41:27 +0530

    1st accordian

---
+++ src/components/Accordian/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import data from "./data";
+
+const Accordian = () => {
+    
+    const [selected, setSelected] = useState(null);
+    
+    return (
+        <div className="wrapper">
+            <div className="accordian">
+                {
+                    data && data.length > 0 ?
+                        data.map(dataItem => (
+                            <div className="item" key={dataItem.id}>
+                                <div className="title">
+                                    <h3>{dataItem.question}</h3>
+                                    <span>+</span>
+                                </div>
+                            </div>
+                        ))
+                        : <div>No data found !</div>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Accordian;
