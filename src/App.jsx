07c8ace098f58bde5c78c70c8ba0bb90ba5a01ba
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-05-23 01:19:55 +0530

    5th poject done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/Accordian";
 import ImageSlider from "./components/ImageSlider";
+import LoadMoreData from "./components/LoadMoreData";
 import RandomColor from "./components/RandomColor";
 import StarRating from "./components/StarRating";
 
@@ -20,10 +21,13 @@ function App() {
       {/* <StarRating NoOfStarts={10} /> */}
 
       {/* Image Slider  */}
-      <ImageSlider
+      {/* <ImageSlider
       url={'https://picsum.photos/v2/list?page=1&limit=5'} 
       page={1} 
-      limit={10} />
+      limit={10} /> */}
+
+      {/* Load More Data */}
+        <LoadMoreData />
     </div>
   );
 }
