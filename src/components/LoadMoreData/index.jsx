4a89dd2312962a433cdfdb6811aedb3b571b8296
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-05-23 00:55:31 +0530

    5th project load more data

--- src/components/LoadMoreData/index.jsx
+++ src/components/LoadMoreData/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function LoadMoreData(){
@@ -6,6 +6,20 @@ export default function LoadMoreData(){
     const [loading, setLoading] = useState(false);
     const [products, setProducts] = useState([]);
     const [count, setCount] = useState(0);
+
+    async function fetchProducts() {
+        try {
+            
+            const response = await fetch('https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}&select=title,price')
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() =>{
+        fetchProducts()
+    },[])
 
     return <div className="container"></div>
 }
